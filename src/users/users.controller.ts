7ad5f2422898bfc119437820193d1f934bb2c952
yommexg <hydrokidb@gmail.com>
2026-02-18 11:17:51 +0000

    learn how controller orks in nest

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // GET/users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'EMPLOYEE' | 'MANAGER') {
+    return [role];
+  }
+
+  @Get(':id') // GET/users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST/users
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH/users/:id
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE/users/:id
+  remove(@Param('id') id: string) {
+    return { id };
+  }
+}
